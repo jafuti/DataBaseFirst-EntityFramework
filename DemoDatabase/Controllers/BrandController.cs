e435a4aa76d3f6adc9902623106fd60995e8b742
jafare2010@gmail.com <jafare2010@gmail.com>
2021-12-03 19:24:31 +0300

    first commit

---
+++ DemoDatabase/Controllers/BrandController.cs
@@ -0,0 +1,24 @@
+﻿using DemoDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoDatabase.Controllers
+{
+    public class BrandController : Controller
+    {
+        // GET: Brand
+        EFDBFirstDatabaseEntities4 _context;
+        public BrandController()
+        {
+            _context = new EFDBFirstDatabaseEntities4();
+        }
+        public ActionResult Index()
+        {
+         var brands =  _context.Brands.ToList();
+            return View(brands);
+        }
+    }
+}
---
+++ DemoDatabase/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoDatabase.Models;
+namespace DemoDatabase.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        EFDBFirstDatabaseEntities4 _context = new EFDBFirstDatabaseEntities4();
+        public ActionResult Index()
+        {
+     var category =  _context.Categories.ToList();
+            return View(category);
+        }
+    }
+}
